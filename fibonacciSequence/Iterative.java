5760e0b8a2bd66280b6306249a72e64a3d5614de
Anav Chand <anavkki@gmail.com>
2025-08-14 19:36:39 +1200

    fix: update iterative fib seq time complexity

--- fibonacciSequence/Iterative.java
+++ fibonacciSequence/Iterative.java
@@ -1,19 +1,18 @@
 public class Iterative {
     public static void main(String[] args) {
-        int[] sizes = {10, 20, 30};
-        // Passes: 10, 30, 60
-        
-        int a = 0, b = 1, counter = 0;
+        int[] sizes = { 10, 20, 30 };
+        // Passes: 10, 20, 30
 
-        for(int n: sizes) {
+        for (int n : sizes) {
+            int a = 0, b = 1, counter = 0;
+
             for (int i = 0; i < n; i++) {
                 counter++;
                 int next = a + b;
                 a = b;
                 b = next;
-
             }
-            
+
             System.out.println(counter);
         }
     }
